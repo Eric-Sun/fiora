4b81f239ca31970afbdc5b0d960d99e2dc8eeeae
Your Name <you@example.com>
2016-09-25 20:29:53 +0800

    add

---
+++ src/main/java/com/j13/fiora/fetcher/Comment.java
@@ -0,0 +1,22 @@
+package com.j13.fiora.fetcher;
+
+public class Comment {
+    private String content;
+    private String id;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
